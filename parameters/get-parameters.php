a03cfb725edf925d0137673a67a7ea82ef875230
Joe Zhuang <joezhuang.au@gmail.com>
2024-04-26 10:28:22 +1000

    parameter update

--- parameters/get-parameters.php
+++ parameters/get-parameters.php
@@ -46,21 +46,24 @@ require __DIR__ . '/vendor/autoload.php';
 // *******************PARAMETER SECTION *************************** //
 
 
-// STEP 1 : STUDENT TO UPDATE - KEVAULT ENDPOINT URL
-// $secret = new AzKeyVault\Secret('https://capstoneproject-kv.vault.azure.net/');
-// $secrets = $secret->getSecrets();
+STEP 1 : STUDENT TO UPDATE - KEVAULT ENDPOINT URL
+$secret = new AzKeyVault\Secret('https://deltatrack-kvault.vault.azure.net/');
+$secrets = $secret->getSecrets();
 
-// STEP 2: STUDENT TO MAKE SURE - PARAMETER NAME BELOW MATCHES WITH THE PARAMETER NAME PROVIDED IN KEYVAULT SETTINGS PAGE IN AZURE PORTAL
-// $host  = $secret->getSecret('kv-db1-host');
-// $username  = $secret->getSecret('kv-db1-username');
-// $password = $secret->getSecret('kv-db1-password');
-// $db_name  = $secret->getSecret('kv-db1-dbname');l
-$host  = 'deltatrack-db.mysql.database.azure.com';
-$username  = 'localadmin';
-$password = 'Joezhuang.au123!';
-$db_name  = 'deltatrack_data';
-// STEP 3: STUDENT TO DOWNLOAD AND UPLOAD PEM  CERT FOR THE DATABASE CREATED  ( From Azure Portal ) to the SSL folder with the same name as below
-$sslcert    = 'ssl/DigiCertGlobalRootCA.crt.pem';
+STEP 2: STUDENT TO MAKE SURE - PARAMETER NAME BELOW MATCHES WITH THE PARAMETER NAME PROVIDED IN KEYVAULT SETTINGS PAGE IN AZURE PORTAL
+$host  = $secret->getSecret('kv-db1-host');
+$username  = $secret->getSecret('kv-db1-username');
+$password = $secret->getSecret('kv-db1-password');
+$db_name  = $secret->getSecret('kv-db1-dbname');
+$sslcert = $secret->getSecret('kv-db1-sslcert');
+
+
+// $host  = 'deltatrack-db.mysql.database.azure.com';
+// $username  = 'localadmin';
+// $password = 'Joezhuang.au123!';
+// $db_name  = 'deltatrack_data';
+// // STEP 3: STUDENT TO DOWNLOAD AND UPLOAD PEM  CERT FOR THE DATABASE CREATED  ( From Azure Portal ) to the SSL folder with the same name as below
+// $sslcert    = 'ssl/DigiCertGlobalRootCA.crt.pem';
 
 
 
